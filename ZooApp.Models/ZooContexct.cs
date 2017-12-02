e2c1f116b0da397255589384932a851a4eb95562
Safayet Hossain <emonsafayet@gmail.com>
2017-12-02 21:28:38 +0600

    create enviroment

---
+++ ZooApp.Models/ZooContexct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace ZooApp.Models
+{
+    public class ZooContext : DbContext
+    {
+        public ZooContext() : base("ZooContext")
+        {
+
+        }
+        public DbSet<Animal> Animals { get; set; }
+    }
+}
